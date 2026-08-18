22491199170ba227b0adcf32f676843a9372c134
Theneuralpeach <yurisa@sizeme.biz>
2026-08-17 20:09:29 -0700

    Sip of Japan tab: 10 IG accounts, free-form groups, optional open-URL on text items

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'links-v1';
+const CACHE = 'links-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
