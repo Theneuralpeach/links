786bdd36bed79c25c3e638ed293fe406dda15e15
Theneuralpeach <yurisa@sizeme.biz>
2026-08-17 23:51:44 -0700

    Account cards: de-emphasize the display-name chip and label it accurately

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'links-v2';
+const CACHE = 'links-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
